49a10bd94f22471d856a465ba20c06eb0b803ce5
Tuniwutzi <wutzelus@web.de>
2020-05-21 21:43:20 +0200

    Finish untested operator implementation

--- jb/unit/unit_operators.hpp
+++ jb/unit/unit_operators.hpp
@@ -8,6 +8,10 @@ namespace jb {
 namespace unit {
         
 
+/**
+ * Compare two units.
+ * Only applicable if one is implicitly convertible to the other.
+ */
 template<typename UNIT_A, typename UNIT_B,
          typename = std::enable_if_t<IsUnitV<UNIT_A> && IsUnitV<UNIT_B> && (UNIT_A::IsImplicitlyConvertibleTo<UNIT_B> || UNIT_B::IsImplicitlyConvertibleTo<UNIT_A>)>>
 bool operator==(const UNIT_A& a, const UNIT_B& b) {
@@ -18,6 +22,14 @@ bool operator==(const UNIT_A& a, const UNIT_B& b) {
     }
 }
 
+/**
+ * Add two units.
+ * Only applicable if they share the same base_unit.
+ * 
+ * Must not be allowed for any implicitly convertible units, as the 0-points might be different (ie celcius and fahrenheit).
+ * 
+ * @return the result of the addition represented in the more exact unit.
+ */
 template<typename UNIT_A, typename UNIT_B,
          typename = std::enable_if_t<IsUnitV<UNIT_A> && IsUnitV<UNIT_B> && std::is_same_v<typename UNIT_A::base_unit, typename UNIT_B::base_unit>>>
 auto operator+(const UNIT_A& a, const UNIT_B& b) {
@@ -34,6 +46,14 @@ auto operator+(const UNIT_A& a, const UNIT_B& b) {
     }
 }
 
+/**
+ * Subtract two units.
+ * Only applicable if they share the same base_unit.
+ * 
+ * Must not be allowed for any implicitly convertible units, as the 0-points might be different (ie celcius and fahrenheit).
+ * 
+ * @return the result of the subtraction represented in the more exact of the two units.
+ */
 template<typename UNIT_A, typename UNIT_B,
          typename = std::enable_if_t<IsUnitV<UNIT_A> && IsUnitV<UNIT_B> && std::is_same_v<typename UNIT_A::base_unit, typename UNIT_B::base_unit>>>
 auto operator-(const UNIT_A& a, const UNIT_B& b) {
@@ -48,6 +68,32 @@ auto operator-(const UNIT_A& a, const UNIT_B& b) {
         return unit_a(a.count() - bConverted.count());
 
     }
+}
+
+/**
+ * Multiply a unit with an arithmetic type or an arithmetic type with a unit.
+ * 
+ * @return the result of the multiplication represented in the unit
+ */
+template<typename TYPE_A, typename TYPE_B,
+         typename = std::enable_if_t<(IsUnitV<TYPE_A> && std::is_arithmetic_v<TYPE_B>) || std::is_arithmetic_v<TYPE_A> && IsUnitV<TYPE_B>>>
+auto operator*(const TYPE_A& a, const TYPE_B& b) {
+    if constexpr (IsUnitV<TYPE_A>) {
+        return a.count() * b;
+    } else {
+        return a * b.count();
+     }
+}
+
+/**
+ * Divide a unit by an arithmetic type.
+ * 
+ * @return the unit divided by the arithmetic value
+ */
+template<typename TYPE_A, typename TYPE_B,
+         typename = std::enable_if_t<(IsUnitV<TYPE_A> && std::is_arithmetic_v<TYPE_B>)>>
+auto operator/(const TYPE_A& a, const TYPE_B& b) {
+    return a.count() / b;
 }
 
 
