9e20016c14d020eb47deb8ec87cce251ccdbb7d7
Tuniwutzi <wutzelus@web.de>
2020-06-09 16:28:14 +0200

    WIP interop

---
+++ jb/unit/chrono_interop.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include "interop.hpp"
+
+#include <chrono>
+
+
+namespace jb {
+namespace unit {
+
+namespace base_units {
+
+struct Second {
+    static inline const char* symbol = "s";
+};
+
+}
+
+namespace interop {
+
+template<typename RATIO, typename REP>
+struct ResolveBaseUnit<std::chrono::duration<RATIO, REP>> {
+    using type = ::jb::unit::base_units::Second;
+};
+
+template<typename RATIO, typename REP>
+struct MakeUnit<::jb::unit::base_units::Second, RATIO, REP>> {
+    using type = std::chrono::duration<RATIO, REP>;
+};
+
+}
+}
+}
